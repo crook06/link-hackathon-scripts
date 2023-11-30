ed4b50cd68d54458a6e875a4128ce525ce76abd1
crook06 <112490805+crook06@users.noreply.github.com>
2023-11-30 01:59:32 +0100

    Update and rename 01_deployConsumers.js to 01_deployConsumers_risk.js

--- scripts/01_deployConsumers_risk.js
+++ scripts/01_deployConsumers_risk.js
@@ -1,27 +1,27 @@
-const { abi, bytecode } = require("../contracts/abi/FunctionsConsumer.json");
+const { abi, bytecode } = require("../contracts/abi/RiskFunctionsConsumer.json");
 const { wallet, signer } = require("../connection.js");
 const { networks } = require("../networks.js");
 const { ContractFactory, utils } = require("ethers");
 
-const NETWORK = "polygonMumbai";
+const NETWORK = "ethereumSepolia";
 
 const routerAddress = networks[NETWORK].functionsRouter;
 const donIdBytes32 = utils.formatBytes32String(networks[NETWORK].donId);
 
-const deployFunctionsConsumerContract = async () => {
+const deployRiskFunctionsConsumerContract = async () => {
   const contractFactory = new ContractFactory(abi, bytecode, wallet);
 
   console.log(
-    `\nDeploying FunctionsConsumer contract on network ${NETWORK}...`
+    `\nDeploying RiskFunctionsConsumer contract on network ${NETWORK}...`
   );
-  const functionsConsumerContract = await contractFactory
+  const riskFunctionsConsumerContract = await contractFactory
     .connect(signer)
     .deploy(routerAddress, donIdBytes32);
 
-  await functionsConsumerContract.deployed();
-  console.log(`\nDeployed at address ${functionsConsumerContract.address}`)
+  await riskFunctionsConsumerContract.deployed();
+  console.log(`\nDeployed at address ${riskFunctionsConsumerContract.address}`)
 };
 
-deployFunctionsConsumerContract().catch(err => {
+deployRiskFunctionsConsumerContract().catch(err => {
   console.log("Error deploying the Consumer Contract ", err);
 });
